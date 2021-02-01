fb6270600ab64d14a4df7feb81c2957d8c394388
jane <m3281@hotmail.com>
2021-02-02 01:03:42 +0800

    fix position

--- app/models/list.rb
+++ app/models/list.rb
@@ -2,6 +2,6 @@ class List < ApplicationRecord
   acts_as_list scope: :user
   belongs_to :user
 
-  has_many :cards, -> { order(positon: :asc)}, dependent: :destroy
+  has_many :cards, -> { order(position: :asc)}, dependent: :destroy
   validates :name, presence: true
 end
